95bcfc47c3b3e2e8bf9aac26c055e46b7dc8e8c9
muhammedazlan11@gmail.com <muhammedazlan11@gmail.com>
2022-10-11 22:43:42 +0530

    co

---
+++ main.js
@@ -0,0 +1,36 @@
+
+function forall(){
+  document.getElementById('fourth').style.display = 'block';
+ document.getElementById('fifth').style.display = 'block';
+}
+
+function forthird(){
+ document.getElementById('fourth').style.display ='none'; 
+ document.getElementById('fifth').style.display = 'none';
+  
+}
+function forfourth(){
+  document.getElementById('fourth').style.display = 'block';
+ document.getElementById('fifth').style.display = 'none';
+}
+
+function forfifth(){
+  
+  document.getElementById('fifth').style.display = 'block';
+ document.getElementById('fourth').style.display = 'none';
+}
+function forsixth() {
+  document.getElementById('fourth').style.display = 'none';
+  document.getElementById('fifth').style.display = 'none';
+
+}
+function forseventh(){
+ document.getElementById('fourth').style.display ='none'; 
+ document.getElementById('fifth').style.display = 'none';
+  
+}
+function forthird() {
+  document.getElementById('fourth').style.display = 'none';
+  document.getElementById('fifth').style.display = 'none';
+
+}
