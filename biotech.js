e674ede383e9e5282f272ed3fbe1df716dbfda37
muhammedazlan11@gmail.com <muhammedazlan11@gmail.com>
2022-10-12 23:54:22 +0530

    euur

---
+++ biotech.js
@@ -0,0 +1,44 @@
+
+ document.getElementById('default').style.display = 'none';
+function forall(){
+  document.getElementById('fourth').style.display = 'block';
+ document.getElementById('fifth').style.display = 'block';
+ document.getElementById('default').style.display = 'none';
+ 
+}
+
+function forthird(){
+  
+ document.getElementById('default').style.display = 'block';
+ document.getElementById('fourth').style.display ='none'; 
+ document.getElementById('fifth').style.display = 'none';
+  
+}
+
+function forfourth(){
+  
+  document.getElementById('fourth').style.display = 'block';
+ document.getElementById('fifth').style.display = 'none';
+}
+
+function forfifth(){
+  
+ document.getElementById('default').style.display = 'none';
+  
+  document.getElementById('fifth').style.display = 'block';
+ document.getElementById('fourth').style.display = 'none';
+}
+function forsixth() {
+  
+ document.getElementById('default').style.display = 'block';
+  document.getElementById('fourth').style.display = 'none';
+  document.getElementById('fifth').style.display = 'none';
+
+}
+function forseventh(){
+  
+ document.getElementById('default').style.display = 'block';
+ document.getElementById('fourth').style.display ='none'; 
+ document.getElementById('fifth').style.display = 'none';
+  
+}
